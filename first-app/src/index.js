462fbaf2e00d23525d04832f51221f7ab23fa106
sandeepsharma99 <sandeepsharma9182@gmail.com>
2025-09-27 23:52:16 +0530

    Problem when upfating data ui is not uodating when we are using function and expression

---
+++ first-app/src/index.js
@@ -0,0 +1,25 @@
+function sayHi(name){
+  return `hi ${name}`
+}
+
+function add(a,b){
+  return  a+b;
+}
+
+function subtract(a,b){
+  return a-b;
+}
+
+function multiply(){
+  return a*b;
+}
+
+function division(){
+  return a/b;
+}
+
+function power(){
+  return a**b;
+}
+
+export {sayHi, add,subtract, multiply, division,power};
--- fistapp/src/App.jsx
+++ fistapp/src/App.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 
 const App = () => {
+  // function clickHandler(){
+  //   console.log("button clicked")
+  let a = 1;
+  function increment(){
+      a = a+1;
+      console.log(a)
+  }
   return (
     <div>
-      app
+      <p>value of a : {a}</p>
+      {/* <button  onClick={clickHandler}>button</button> {clickhandler} is just reference of the function and we define function above return obviously} */}
+      <button onClick={increment}>click</button>
     </div>
   )
-}
+  }
+
 
 export default App
