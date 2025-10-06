2ef6a70c2be6f7204a688634e7c5244c2199c102
sandeepsharma99 <sandeepsharma9182@gmail.com>
2025-10-06 21:25:12 +0530

    use memo stores the repeated value untill new change is called

--- usememo/src/App.jsx
+++ usememo/src/App.jsx
@@ -1,34 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import { useMemo } from 'react';
+import { useState } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
 
+const App = () => {
+  const [count,setCount]=useState(0)
+  
+  function sum(){
+    console.log("i'm heavy task done by me")
+    let sum = 0;
+    for(let i=0;i<1000000000;i++){
+      sum = sum+i;
+    }
+    return sum;
+  }
+  const res = useMemo(sum,[])
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <p>result of heavy Computation : {res}</p>
+      count : {count}<br></br>
+      <button onClick={()=>{setCount(count+1)}}>Increase</button>
+    </div>
   )
 }
 
