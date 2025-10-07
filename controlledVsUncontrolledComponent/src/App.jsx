46b00ae0ccb2289e824bdcfad1ff6719d32bd9f5
sandeepsharma99 <sandeepsharma9182@gmail.com>
2025-10-07 22:40:14 +0530

    uncontrolled component using useref

--- controlledVsUncontrolledComponent/src/App.jsx
+++ controlledVsUncontrolledComponent/src/App.jsx
@@ -21,3 +21,28 @@
 // }
 
 // export default App
+
+import React, { useRef } from 'react'
+
+
+const App = () => {
+  const ref = useRef()
+  const  handleSubmit = (e)=>{
+    e.preventDefault()
+    alert(`you've typed ${ref.current.value}`) 
+  }
+  return (
+    <div>
+      <form action="" onSubmit={handleSubmit}>
+        <fieldset>
+          <label htmlFor="">Text : </label>
+          <input type="text" ref ={ref} />
+          <button>submit</button>
+        </fieldset>
+      </form>
+    </div>
+  )
+}
+
+export default App
+
