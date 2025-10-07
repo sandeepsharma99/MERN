a6019434b6a3aaac2b51dc04e1f841b96062c889
sandeepsharma99 <sandeepsharma9182@gmail.com>
2025-10-07 22:18:40 +0530

    controllred component using useState

---
+++ controlledVsUncontrolledComponent/src/App.jsx
@@ -0,0 +1,23 @@
+// /* Controlled component : we use useState in control Component */
+
+// import React, { useState } from 'react'
+
+// const App = () => {
+//   const [name,setName ] = useState("")
+//   function handleChange(e){
+//     setName(e.target.value)
+//   }
+//   return (
+//     <div>
+//       <p>Name : {name}</p>
+//       <form action="">
+//         <fieldset>
+//         <label htmlFor="">name : </label>
+//         <input type="text" onChange={handleChange} />
+//         </fieldset>
+//       </form>
+//     </div>
+//   )
+// }
+
+// export default App
