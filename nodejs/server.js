56458c15752da766f2211b01aa19085fcc73ff86
sandeepsharma99 <sandeepsharma9182@gmail.com>
2025-10-20 19:18:34 +0530

    server side rendering using fs module async operation

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,11 +1,52 @@
-import http from "http";
+/* cretaing server using Http module */
 
-const PORT  = 3000;
+// import http from "http"
 
-const server = http.createServer((req,res)=>{
-    res.end("hello from server")
+// const PORT = 3000;
+// const server = http.createServer((req,res)=>{
+//     if(req.url == '/'){
+//         res.end("server")
+//     }else if(req.url == "/about"){
+//          res.end("About Page")
+//     }else if(req.url == "/conatct"){
+//          res.end("Contact Page :)")
+//     }
+// });
+
+
+// server.listen(PORT,()=>{
+//     console.log("server is listening on port 3000")
+// })
+
+
+
+/* creating server using  express
+1. Install express 
+*/
+
+import express from "express";
+
+import fs from "fs"
+
+const PORT = 3000;
+const server = express();
+
+server.get("/",(req,res)=>{
+    res.end("Hi from serverr...")
 })
 
-server.listen(PORT,()=>{
-    console.log(`our server is running on localhost 3000 :)`)
+server.get("/about",(req,res)=>{
+// i'll read file using fs module asyn operation...
+    fs.readFile("./about.html","utf-8",(err,data)=>{
+        if(err){
+            res.status(500).send("err in reading file")
+            return;
+        }
+        res.send(data)
+    });
+})
+
+
+server.listen(PORT, ()=>{
+    console.log("server is listeniing on port 3000")
 })
