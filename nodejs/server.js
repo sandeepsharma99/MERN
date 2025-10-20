9651b00449e554eedb63068a92aa07295ebeea11
sandeepsharma99 <sandeepsharma9182@gmail.com>
2025-10-20 16:42:33 +0530

    server

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,1 +1,11 @@
-console.log("hello world")
+import http from "http";
+
+const PORT  = 3000;
+
+const server = http.createServer((req,res)=>{
+    res.end("hello from server")
+})
+
+server.listen(PORT,()=>{
+    console.log(`our server is running on localhost 3000 :)`)
+})
