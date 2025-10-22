15685a5dd5f2373f13542f0930df0908896f70ed
sandeepsharma99 <sandeepsharma9182@gmail.com>
2025-10-22 19:43:52 +0530

    deleted e commerce

--- Ecommerce/src/App.jsx
+++ Ecommerce/src/App.jsx
@@ -5,6 +5,7 @@ import { Route, Routes  } from  "react-router-dom";
 import About from './Landing_page/About'
 import Cart from './Landing_page/Cart'
 import Product from './Landing_page/product';
+import { CartProvider } from './context/CartContext';
 
 
 
