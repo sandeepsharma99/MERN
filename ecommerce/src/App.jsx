94a0e4b9cb441ba613ffe024d7d7b95020c3e883
sandeepsharma99 <sandeepsharma9182@gmail.com>
2025-10-22 21:00:18 +0530

    basic navigation created

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -1,33 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import Navbar from './Landing_page/Navbar'
+import { BrowserRouter, Routes,Route } from 'react-router-dom'
+import Home from './Landing_page/Home'
+import Product from './Landing_page/Product'
+import About from './Landing_page/About'
+import Cart from './Landing_page/Cart'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <BrowserRouter>
+    <Navbar/>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/about" element={<About/>}/>
+      <Route path="/product" element={<Product/>}/>
+      <Route path="/cart" element={<Cart/>}/>
+    </Routes>
+    </BrowserRouter>
+      
     </>
   )
 }
---
+++ ecommerce/src/Landing_page/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <>
+      <div className='container'>
+        <h1>Ecommerce</h1>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/product">Product</Link>
+        <Link to="/cart">Cart</Link>
+      </div>
+    </>
+  )
+}
+
+export default Navbar
