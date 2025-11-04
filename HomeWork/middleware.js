790635759a628416cc62648557a43dda103df526
sandeepsharma99 <sandeepsharma9182@gmail.com>
2025-11-04 19:25:44 +0530

    middleware practice

--- HomeWork/middleware.js
+++ HomeWork/middleware.js
@@ -2,6 +2,10 @@ import express from "express"
 const PORT = 3000
 const server = express()
 
+app.get("/",(req,res)=>{
+    res.send("hello world")
+})
+
 server.listen(PORT ,()=>{
     console.log("server initiated")
 })
