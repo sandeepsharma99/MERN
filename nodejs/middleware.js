fd448917f59dde90f1274d7ee158c8799a322aae
sandeepsharma99 <sandeepsharma9182@gmail.com>
2025-11-04 19:23:01 +0530

    practicising middleware

--- nodejs/middleware.js
+++ nodejs/middleware.js
@@ -1,39 +1,38 @@
-import express from "express";
+// import express from "express";
 
-const PORT = 3000;
+// const PORT = 3000;
 
-const app = express();
+// const app = express();
 
-//authanticate
+// //authanticate
 
-app.use((req,res,next)=>{
-     let password = 1234567;
-    if(req.body.password.length == 6){
-        console.log("valid user")
-    }else{
+// app.use((req,res,next)=>{
+//      let password = 1234567;
+//     if(req.body.password.length == 6){
+//         console.log("valid user")
+//     }else{
 
-    }
-})
-next();
+//     }
+// })
+// next();
 
-app.use((req,res,next)=>{
-    if(req.body.username === username && req.body.password === password && role === role){ 
-    }
-    {
-        console.log("middleware  1 called") 
-        next()
-    }
-})
+// app.use((req,res,next)=>{
+//     if(req.body.username === username && req.body.password === password && role === role){ 
+//     }
+//     {
+//         console.log("middleware  1 called") 
+//         next()
+//     }
+// })
 
-app.get("/",(req,res)=>{
-    res.render("admin")
-})
+// app.get("/",(req,res)=>{
+//     res.render("admin")
+// })
 
-app.post("/login",(req,res)=>{
-    res.send("Login page")
-})
+// app.post("/login",(req,res)=>{
+//     res.send("Login page")
+// })
 
-app.listen(PORT,(req,res)=>{
-    console.log("server is listening on 3000")
-})
-
+// app.listen(PORT,(req,res)=>{
+//     console.log("server is listening on 3000")
+// })
