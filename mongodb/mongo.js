43b475ae4954e960401d013f861d7a87c93584dd
sandeepsharma99 <sandeepsharma9182@gmail.com>
2025-11-06 17:35:23 +0530

    database connected

--- mongodb/mongo.js
+++ mongodb/mongo.js
@@ -9,44 +9,68 @@ const app=express();
 
 app.use(express.json());
 
-mongoose.connect(process.env.MONGODB_URL).then(()=>{
-    console.log("connected to mongodb");
+mongoose.connect(process.env.MONGODB_URL).then(()=>{     // returns promises and we handle promise s=using .then.catch
+    console.log("connected to mongodb"); 
     
 }).catch((err)=>{
-    console.log(err);
+    console.log(err);   
     
 })
 
 // create operation
+//schema
+const userSchema = new mongoose.Schema({ // creating schema
+    email: String,
+    password: String,
+})
+//creating model 
+const User =  mongoose.model("User",userSchema)
 
-const userSchema= new mongoose.Schema({
-    email:String,
-    password:String,
-});
-
-const User = mongoose.model("User",userSchema);
-
-app.post("/allusers",async (req,res)=>{
+app.post("/signup",async (req,res)=>{
     try{
-        const {email,password}=req.body;
-        const user= new User(req.body);
-
-        const saltRounds = 10;
-        const myPlaintextPassword = 'mongodb+srv://sandeepsharma9182:sandeep123@e-comm.fokiduj.mongodb.net/';
-
-        bcrypt.genSalt(saltRounds, function(err, salt) {
-        bcrypt.hash(myPlaintextPassword, salt);
-        });
-
-        const savedUSer=await user.save();
-        res.status(201).json({message:"user created sucessfully"});
-        console.log(req.body);
-        
+        const email = req.body.email;
+        const password = req.body.password;
+        // const{email,password}=req.body;
+        const user = new User({email:email,password:password});
+        const savedUser = await user.save();
+        res.status(201).json({message:"user created successfully"});
     }catch(err){
-        console.log("error",err);
-        
+        console.log(err)
     }
 })
+app.listen((PORT),()=>{
+    console.log(`server stared on the PORT ${PORT}`);
+    
+})
+
+// const userSchema= new mongoose.Schema({
+//     email:String,
+//     password:String,
+// });
+
+// const User = mongoose.model("User",userSchema);
+
+// app.post("/allusers",async (req,res)=>{
+//     try{
+//         const {email,password}=req.body;
+//         const user= new User(req.body);
+
+//         // const saltRounds = 10;
+//         // const myPlaintextPassword = 'mongodb+srv://sandeepsharma9182:sandeep123@e-comm.fokiduj.mongodb.net/';
+
+//         // bcrypt.genSalt(saltRounds, function(err, salt) {
+//         // bcrypt.hash(myPlaintextPassword, salt);
+//         // });
+
+//         const savedUSer=await user.save();
+//         res.status(201).json({message:"user created sucessfully"});
+//         console.log(req.body);
+        
+//     }catch(err){
+//         console.log("error",err);
+        
+//     }
+// })
 
 // read opeartion
 // app.get("/allusers",async(req,res)=>{
@@ -55,27 +79,23 @@ app.post("/allusers",async (req,res)=>{
 //     }
 // })
 
-app.get ("/allusers/:id",async (req,res)=>{
-    try{
-        const user  = await User.findById(req.params.id);
-        if(!user) return res.status(404).json({message:"usernot found"})
-        res.json(user)
-    }catch(err){
-        res.status(400).json({message: "something went wrong"})
-    }
-})
+// app.get ("/allusers/:id",async (req,res)=>{
+//     try{
+//         const user  = await User.findById(req.params.id);
+//         if(!user) return res.status(404).json({message:"usernot found"})
+//         res.json(user)
+//     }catch(err){
+//         res.status(400).json({message: "something went wrong"})
+//     }
+// })
 
-app.put("/alluser/login",async(req,res)=>{
-    try{
+// app.put("/alluser/login",async(req,res)=>{
+//     try{
 
-    }catch{
-        res.status
-    }
-})
+//     }catch{
+//         res.status
+//     }
+// })
 
-app.delete("/alluser")
+// app.delete("/alluser")
 
-app.listen((PORT),()=>{
-    console.log("server stared");
-    
-})
