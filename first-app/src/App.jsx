930ff74145d2d7568a115230aff2de5787fdca47
sandeepsharma99 <sandeepsharma9182@gmail.com>
2026-03-19 13:34:12 +0530

    updated

--- first-app/src/App.jsx
+++ first-app/src/App.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import Navbar from './Navbar'
+import Home from './Home'
+
+
 
 const App = () => {
   return (
+    <>
     <div>snadeep sharma Oo</div>
+    <Navbar/>
+    <Home/>
+    </>
   )
 }
 
